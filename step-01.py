3381b8dd6eb388e8fc1660b35fb0d4c5ac39e874
Deathspike <meister.deathspike@outlook.com>
2023-05-27 20:29:40 +0200

    Add input extension filter

--- step-01.py
+++ step-01.py
@@ -1,10 +1,12 @@
 import config
 import os
+import re
 import subprocess
 
 for inputName in os.listdir(config.inputPath):
-    os.makedirs(config.framePath, exist_ok=True)
-    inputPath = os.path.join(config.inputPath, inputName)
-    frameName = f'{inputName}-%06d.png'
-    framePath = os.path.join(config.framePath, frameName)
-    subprocess.run(['ffmpeg', '-i', inputPath, '-vf', 'select=eq(pict_type\\,I)', '-vsync', 'vfr', framePath], check=True)
+    if re.search(r'\.(avi|mp4|mkv|ogm|webm)$', inputName):
+        os.makedirs(config.framePath, exist_ok=True)
+        inputPath = os.path.join(config.inputPath, inputName)
+        frameName = f'{inputName}-%06d.png'
+        framePath = os.path.join(config.framePath, frameName)
+        subprocess.run(['ffmpeg', '-i', inputPath, '-vf', 'select=eq(pict_type\\,I)', '-vsync', 'vfr', framePath], check=True)
